ad39adb8b918d259a8c7985c97a8e94b99a2032f
Manomenu <88382777+Manomenu@users.noreply.github.com>
2024-08-11 23:13:40 +0200

    [TC-11] simple udp connection with esp

---
+++ TractorSupporter/TSWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace TractorSupporter
+{
+    /// <summary>
+    /// Interaction logic for TSWindow.xaml
+    /// </summary>
+    public partial class TSWindow : Window
+    {
+        public TSWindow()
+        {
+            InitializeComponent();
+
+            setMyIP();
+
+            Thread thdUdpServer = new Thread(new ThreadStart(serverThread));
+
+            thdUdpServer.IsBackground = true;
+            thdUdpServer.Start();
+        }
+
+        private void setMyIP()
+        {
+            string hostName = Dns.GetHostName();
+            string myIP = Dns.GetHostEntry(hostName)
+                .AddressList.First(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
+            tb_MyIP.Text = myIP;
+        }
+
+        string data = "";
+        string date = "";
+
+
+        public void serverThread()
+        {
+            UdpClient udpClient = new UdpClient(8080);
+            while (true)
+            {
+                IPEndPoint RemoteIpEndpoint = new IPEndPoint(IPAddress.Any, 8080);
+                Byte[] receivedBytes = udpClient.Receive(ref RemoteIpEndpoint);
+                string returnData = Encoding.ASCII.GetString(receivedBytes);
+
+                Dispatcher.Invoke(() =>
+                {
+                    date = DateTime.Now.ToString("hh:mm:ss");
+                    data = "time: " + date + " | " + returnData;
+
+                    tb_IPSender.Text = RemoteIpEndpoint.Address.ToString();
+                    tb_IPDestination.Text = tb_IPSender.Text;
+
+                    tb_ReceivedMessages.AppendText(data + "\n");
+                    tb_ReceivedMessages.ScrollToEnd();
+                });
+            }
+        }
+
+        private void b_SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(tb_IPDestination.Text, Convert.ToInt16(tb_Port.Text));
+            Byte[] dataToSend = Encoding.ASCII.GetBytes(tb_SendMessage.Text);
+            udpClient.Send(dataToSend, dataToSend.Length);
+        }
+    }
+}
