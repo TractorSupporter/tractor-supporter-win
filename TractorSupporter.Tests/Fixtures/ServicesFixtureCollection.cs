8f5a58cce8a48c5f540c90bf5641eb81cabcaa40
Manomenu <88382777+Manomenu@users.noreply.github.com>
2024-11-15 18:34:49 +0100

    TC-86 added unit tests project

---
+++ TractorSupporter.Tests/Fixtures/ServicesFixtureCollection.cs
@@ -0,0 +1,12 @@
+﻿using TractorSupporter.Services;
+
+namespace TractorSupporter.Tests.Fixtures;
+
+[CollectionDefinition("Services")]
+public class ServicesFixtureCollection : ICollectionFixture<ServicesFixture> {}
+
+public class ServicesFixture
+{
+    public AvoidingService AvoidingService => AvoidingService.Instance;
+    public AlarmService AlarmService => AlarmService.Instance;
+}
---
+++ TractorSupporter.Tests/TestClasses/AvoidingServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using TractorSupporter.Tests.Fixtures;
+
+namespace TractorSupporter.Tests.TestClasses;
+
+[Collection("Services")]
+public class AvoidingServiceTests
+{
+    private readonly ServicesFixture _servicesFixture;
+
+    public AvoidingServiceTests(ServicesFixture servicesFixture)
+    {
+        _servicesFixture = servicesFixture;
+    }
+
+    [Theory]
+    [InlineData(1000.0, 500)]
+    [InlineData(3000.0, 500)]
+    [InlineData(2000.0, 500)]
+    [InlineData(4000.0, 500)]
+    [InlineData(8000.0, 500)]
+    public void MakeAvoidingDecisionForSmallerAvoidingDistance_ReturnsTrue(double distanceMeasured, double avoidingDistance)
+    {
+        // Arrange
+        var service = _servicesFixture.AvoidingService;
+        service.AvoidingDistance = avoidingDistance;
+
+        // Act
+        var decision = service.MakeAvoidingDecision(distanceMeasured);
+
+        // Assert
+        decision.Should().BeFalse();
+    }
+}
