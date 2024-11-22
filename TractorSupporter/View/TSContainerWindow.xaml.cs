bbe7da660bea35794b39eec041c98ea8c3060e9d
HaZeL-spc <kacper.leszczu8@gmail.com>
2024-11-22 16:03:02 +0100

    [TC-92] first version

---
+++ TractorSupporter/View/TSContainerWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using TractorSupporter.Services;
+using TractorSupporter.ViewModel;
+using System.Windows;
+
+namespace TractorSupporter.View
+{
+    public partial class TSContainerWindow : Window
+    {
+        public TSContainerWindow()
+        {
+            InitializeComponent();
+            NavigationService.Initialize(MainFrame);
+            DataContext = new TSContainerWindowViewModel();
+        }
+
+        private void MainFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (DataContext is TSContainerWindowViewModel viewModel)
+            {
+                switch (e.Content)
+                {
+                    case StarterPage:
+                        viewModel.IsSettingsVisible = false;
+                        break;
+                    case SettingsPage:
+                        viewModel.IsSettingsVisible = true;
+                        break;
+                    case MainPage:
+                        viewModel.IsSettingsVisible = true;
+                        break;
+                    default:
+                        viewModel.IsSettingsVisible = true;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ TractorSupporter/ViewModel/TSContainerWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TractorSupporter.Model;
+using TractorSupporter.Services.Interfaces;
+using TractorSupporter.Services;
+
+namespace TractorSupporter.ViewModel
+{
+    public class TSContainerWindowViewModel: BaseViewModel
+    {
+        private INavigationService _navigationService;
+        private LanguageService _languageService;
+        private SettingsVisibilityService _settingsVisibilityService;
+
+        public TSContainerWindowViewModel()
+        {  
+            _navigationService = NavigationService.Instance;
+            _languageService = LanguageService.Instance;
+            _settingsVisibilityService = SettingsVisibilityService.Instance;
+            _settingsVisibilityService.PropertyChanged += OnSettingsVisibilityServicePropertyChanged;
+            NavigateToMainPageIfConfigExists();
+            IsSettingsVisible = false;
+        }
+
+        public bool IsSettingsVisible
+        {
+            get => _settingsVisibilityService.IsSettingsVisible;
+            set => _settingsVisibilityService.IsSettingsVisible = value;
+        }
+
+        private void NavigateToMainPageIfConfigExists()
+        {
+            var configAppJson = ConfigAppJson.Instance;
+            AppConfig appConfig = configAppJson.ReadJson();
+
+            if (appConfig != null)
+            {
+                _languageService.ChangeLanguage(appConfig.Language);
+                _navigationService.NavigateToMain();
+            } else
+            {
+                _navigationService.NavigateToStarter();
+            }
+        }
+
+        private void OnSettingsVisibilityServicePropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == nameof(_settingsVisibilityService.IsSettingsVisible))
+            {
+                OnPropertyChanged(nameof(IsSettingsVisible));
+            }
+        }
+    }
+}
