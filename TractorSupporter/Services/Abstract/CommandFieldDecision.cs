c30a54b21947c500826bd076e25bc71663f4ac47
Manomenu <88382777+Manomenu@users.noreply.github.com>
2024-12-11 14:49:56 +0100

    min distance added

--- TractorSupporter/Services/Abstract/CommandFieldDecision.cs
+++ TractorSupporter/Services/Abstract/CommandFieldDecision.cs
@@ -1,14 +1,18 @@
-﻿namespace TractorSupporter.Services.Abstract;
+﻿using System.Configuration;
+
+namespace TractorSupporter.Services.Abstract;
 
 public abstract class CommandFieldDecision
 {
+    private readonly int _minDistance = int.Parse(ConfigurationManager.AppSettings["MinDistance"] ?? "0");
+
     public bool MakeDecision(double distanceMeasured, List<DateTime> distanceTimes, double distance, int validLifetimeMs, int minSignalsCount)
     {
         var currentTime = DateTime.Now;
 
         distanceTimes.RemoveAll(time => (currentTime - time).TotalMilliseconds > validLifetimeMs);
 
-        if (distanceMeasured <= distance)
+        if (distanceMeasured <= distance && distance >= _minDistance)
             distanceTimes.Add(currentTime);
 
         var decision = distanceTimes.Count >= minSignalsCount;
